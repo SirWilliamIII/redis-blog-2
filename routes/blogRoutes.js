ec07351c7d2a34a3d7a4ea2ce7570b59a2e0f49a
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-04 14:17:18 -0500

    added promisify util function for redis client

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,22 +4,30 @@ const requireLogin = require('../middlewares/requireLogin')
 const Blog = mongoose.model('Blog')
 
 module.exports = app => {
-	app.get('/api/blogs/:id', requireLogin, async(req, res) => {
+	app.get('/api/blogs/:id', requireLogin, async (req, res) => {
 		const blog = await Blog.findOne({
 			_user: req.user.id,
-			_id:   req.params.id
+			_id: req.params.id
 		})
 
 		res.send(blog)
 	})
 
-	app.get('/api/blogs', requireLogin, async(req, res) => {
+	app.get('/api/blogs', requireLogin, async (req, res) => {
+		const redis = require('redis')
+		const redisURL = 'redis://127.0.0.1:6379'
+		const client = redis.createClient(redisURL)
+		const util = require('util')
+		client.get = util.promisify(client.get)
+
+		const cachedBlogs = client.get(req.user.id)
+
 		const blogs = await Blog.find({ _user: req.user.id })
 
 		res.send(blogs)
 	})
 
-	app.post('/api/blogs', requireLogin, async(req, res) => {
+	app.post('/api/blogs', requireLogin, async (req, res) => {
 		const { title, content } = req.body
 
 		const blog = new Blog({
@@ -31,7 +39,7 @@ module.exports = app => {
 		try {
 			await blog.save()
 			res.send(blog)
-		} catch(err) {
+		} catch (err) {
 			res.send(400, err)
 		}
 	})
