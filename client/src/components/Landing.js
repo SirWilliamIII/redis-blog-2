6fc70e01a3ca1b88e0c75de800e9923278f8026f
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-04 12:45:15 -0500

    init commit, old blog project w/ google auth

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+	return (
+		<div style={ { textAlign: 'center' } }>
+			<h1>
+				My Blog Site
+			</h1>
+			Write personal blogs
+		</div>
+	)
+}
+
+export default Landing
