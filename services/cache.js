48e57400b2ba496d4d5a82e6c20e8e61fef20dd1
Will Carpenter <willcarpenteriii@gmail.com>
2018-04-04 19:12:04 -0500

    refactored redis code to cache file

--- services/cache.js
+++ services/cache.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+const redisURL = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisURL)
+
+client.get = util.promisify(client.get)
+
 const exec = mongoose.Query.prototype.exec
 
-mongoose.Query.prototype.exec = function() {
-	console.log('About to run QUERY')
-	console.log(this.getQuery())
+mongoose.Query.prototype.exec = async function() {
+	const key = JSON.stringify(
+		Object.assign({}, this.getQuery(), {
+			collection: this.mongooseCollection.name
+		})
+	)
+	// Does value for 'key' exist?
+	const cacheValue = await client.get(key)
+	// Yes
+	if (cacheValue) {
+		console.log(cacheValue)
+	}
 
-	return exec.apply(this, arguments)
+	const result = await exec.apply(this, arguments)
+	client.set(key, JSON.stringify(result))
+
+	return result
 }
