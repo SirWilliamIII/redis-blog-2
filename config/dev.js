1bac2b54cec10224f42b766ae81add2daace763f
SirWilliamIII <dev.wtc3@gmail.com>
2018-04-11 12:04:30 -0500

    no change

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID:
+		                '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+	googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+	googleAPIKey:       'AIzaSyBhK2XoTpvBU8oaLyy3k1klLQp5WlBdTno',
+	mongoURI:           'mongodb://will:redis09876@ds123129.mlab.com:23129/redis-blog',
+	cookieKey:          'redis09876'
+};
